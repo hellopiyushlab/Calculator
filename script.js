c370f9b899b5985dc27a4ae00523c274a4b6b025
Piyush <hellopiyush.lab@gmail.com>
2025-09-26 20:40:41 +0530

    cleaned things up a bit

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function multiply(firstNum, secondNum) {
 function divide(firstNum, secondNum) {
     return firstNum / secondNum;
 }
+
+var firstNum =      2222323222;
+var secondNum =     3444232344;
+var operator =      "+";
+
+const firstNumDisplay =     document.querySelector("#firstNum");
+const operatorDisplay =     document.querySelector("#operator");
+const secondNumDisplay =    document.querySelector("#secondNum");
+
+firstNumDisplay.textContent =   `${firstNum}`;
+operatorDisplay.textContent =   `${operator}`;
+secondNumDisplay.textContent =  `${secondNum}`;
